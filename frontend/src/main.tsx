9c421981625ca2c71ec614ab470fac73a376e2be
EmhaHasyim <redeath001@gmail.com>
2025-03-19 02:59:48 +0700

    add a header, footer and theme for dark light

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Header from './components/layout/Header.tsx'
+import Footer from './components/layout/Footer.tsx'
+import { ThemeProvider } from "@/components/darkMode/themeProvider.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Header />
+      <App />
+      <Footer />
+    </ThemeProvider>
   </StrictMode>,
 )
