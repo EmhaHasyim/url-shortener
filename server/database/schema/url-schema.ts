13cb7fc6ecf3abbdb2dd3d14d6c68dacf84520c2
EmhaHasyim <redeath001@gmail.com>
2025-03-23 00:52:02 +0700

    edit schema to add index on shorturl

--- server/database/schema/url-schema.ts
+++ server/database/schema/url-schema.ts
@@ -1,5 +1,5 @@
 import { sql } from "drizzle-orm";
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, uniqueIndex } from "drizzle-orm/sqlite-core";
 import { createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 
@@ -8,15 +8,16 @@ const urls = sqliteTable('urls', {
     shortUrl: text('short_url').unique().notNull(),
     originalUrl: text('original_url').notNull(),
     createdAt: text('created_at').notNull().default(sql`(CURRENT_TIMESTAMP)`),
-})
-
-const urlSchema = createSelectSchema(urls);
+}, (table) => [
+    uniqueIndex("shortUrlIdx").on(table.shortUrl)
+]
+)
 
 const urlInsertSchema = createSelectSchema(urls, {
     shortUrl: (schema) => schema.min(3).max(20),
     originalUrl: (schema) => schema.url()
 }).omit({ id: true, createdAt: true })
 
-export { urlInsertSchema, urlSchema, urls }
+export { urlInsertSchema, urls }
 
 export type urlInsertType = z.infer<typeof urlInsertSchema>;
