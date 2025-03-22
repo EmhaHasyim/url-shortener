8691712923b2915317373e7c1dbf2771deedf032
EmhaHasyim <redeath001@gmail.com>
2025-03-23 00:50:56 +0700

    add serve static, fix structure, and fix error handling

--- server/index.ts
+++ server/index.ts
@@ -3,36 +3,49 @@ import { RegExpRouter } from 'hono/router/reg-exp-router'
 import indexRoute from './routes/indexRoutes'
 import { HTTPException } from 'hono/http-exception'
 import { ZodError } from 'zod'
+import { serveStatic } from 'hono/bun'
+import { SQLiteError } from "bun:sqlite";
 
 const app = new Hono({ router: new RegExpRouter() })
-    .route('', indexRoute)
-    .onError(async (err, c) => {
-        if (err instanceof HTTPException) {
-            c.status(err.status)
-            if (err.message === 'Url tidak Ditemukan') {
-                return c.redirect('http://localhost:3000')
-            }
-            return c.json({
-                errors: {
-                    message: err.message
-                }
-            })
-        } else if (err instanceof ZodError) {
-            c.status(400)
-            return c.json({
-                errors: {
-                    message: err.message
-                }
-            })
-        } else {
-            c.status(500)
-            return c.json({
-                errors: {
-                    message: err.message
-                }
-            })
+const appRoute = app.route('', indexRoute)
+app.use('/*', serveStatic({ root: '/dist' }))
+app.use('/*', serveStatic({ path: '/dist/index.html' }))
+app.onError(async (err, c) => {
+    if (err instanceof HTTPException) {
+        c.status(err.status)
+        if (err.message === 'Url tidak Ditemukan') {
+            return c.redirect('http://localhost:3000')
         }
-    })
+        return c.json({
+            errors: {
+                message: err.message
+            }
+        })
+    } else if (err instanceof ZodError) {
+        c.status(400)
+        return c.json({
+            errors: {
+                message: err.message
+            }
+        })
+    } else if (err instanceof SQLiteError) {
+        c.status(400)
+        return c.json({
+            erros: {
+                name: err.name,
+                message: err.message
+            }
+        })
+    }
+    else {
+        c.status(500)
+        return c.json({
+            errors: {
+                message: err.message
+            }
+        })
+    }
+})
 
 export default app
-export type AppType = typeof app
+export type AppType = typeof appRoute
