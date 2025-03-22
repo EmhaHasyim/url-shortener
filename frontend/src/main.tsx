1a433ba9e446c258b8635bea25ed366c698e66a2
EmhaHasyim <redeath001@gmail.com>
2025-03-22 23:03:37 +0700

    add query client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,18 @@ import App from './App.tsx'
 import Header from './components/layout/Header.tsx'
 import Footer from './components/layout/Footer.tsx'
 import { ThemeProvider } from "@/components/darkMode/themeProvider.tsx"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Header />
-      <App />
-      <Footer />
-    </ThemeProvider>
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Header />
+        <App />
+        <Footer />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode >
 )
