b4c3e849fb03934f7c22b0d11754b2638f6afcad
EmhaHasyim <redeath001@gmail.com>
2025-03-23 00:51:25 +0700

    add database name when .env blank

--- server/database/db.ts
+++ server/database/db.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 
-const sqlite = new Database(process.env.DB_FILE_NAME);
+const sqlite = new Database(process.env.DB_FILE_NAME || 'database.sqlite');
 export const db = drizzle(sqlite);
