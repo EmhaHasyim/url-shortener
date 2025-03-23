eec72143c682527dc93dc0190ef71170c410efe5
EmhaHasyim <redeath001@gmail.com>
2025-03-24 01:35:00 +0700

    fix typo cause error database

--- server/database/migrate.ts
+++ server/database/migrate.ts
@@ -3,6 +3,6 @@ import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 
-const sqlite = new Database(process.env.DB_FILE_NAME || 'databse.sqlite');
+const sqlite = new Database(process.env.DB_FILE_NAME || 'database.sqlite');
 const db = drizzle(sqlite);
 migrate(db, { migrationsFolder: "./drizzle" });
