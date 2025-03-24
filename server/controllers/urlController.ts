9bfb305fd3abcc6cf378c484d7ff3cca04bb2893
EmhaHasyim <redeath001@gmail.com>
2025-03-24 23:54:30 +0700

    add chache

--- server/controllers/urlController.ts
+++ server/controllers/urlController.ts
@@ -2,14 +2,18 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { urlInsertSchema } from "../database/schema/url-schema";
 import { insertUrlService, selectUrlService } from "../services/urlService";
+import { cache } from 'hono/cache'
 
 const urlControler = new Hono()
-    .get('/:shortUrl', async (c) => {
+    .get('/:shortUrl', cache({
+        cacheName: 'shortUrl',
+        cacheControl: 'max-age=600',
+    }), async (c) => {
         const param = c.req.param('shortUrl')
         const originalUrl = await selectUrlService(param)
         return c.redirect(originalUrl, 301);
     })
-    .post('api/url', zValidator('json', urlInsertSchema), async (c) => {
+    .post('/api/url', zValidator('json', urlInsertSchema), async (c) => {
         const urlData = c.req.valid('json')
         const insertUrl = await insertUrlService(urlData)
         return c.json(insertUrl, 201)
